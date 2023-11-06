6a953df35e0c6c5d9f3ce045448f5880e97fb87d
adeka stanley <adekastanley2@gmail.com>
2023-11-06 13:39:13 +0100

    edited checkout

--- form.js
+++ form.js
@@ -5,10 +5,16 @@ subform.addEventListener("submit", (e) => {
 	e.preventDefault();
 
 	// Check if the form fields are filled
-	const nameInput = document.querySelector("#name");
-	const subPlansSelect = document.querySelector("#sub-plans");
+	const nameInput = document.querySelector("#fname");
+	// const subPlansSelect = document.querySelector("#sub-plans");
+	const lname = document.querySelector("#lname");
+	const email = document.querySelector("#email");
 
-	if (nameInput.value.trim() !== "" && subPlansSelect.value !== "0") {
+	if (
+		nameInput.value.trim() !== "" &&
+		lname.value.trim() !== "" &&
+		email.value.trim() !== ""
+	) {
 		aText.style.display = "flex";
 		subform.reset();
 	} else {
