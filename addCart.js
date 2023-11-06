b6b8fab21aafdf8f4bf4621c7d476368d6979b5e
adeka stanley <adekastanley2@gmail.com>
2023-11-06 09:58:46 +0100

    working on cart logic

---
+++ addCart.js
@@ -0,0 +1,14 @@
+const addCartBtns = document.querySelectorAll(".add-cart ");
+
+addCartBtns.forEach((cartBtn) => {
+	cartBtn.addEventListener("click", () => {
+		// console.log(cartBtn.parentElement);
+		const price = cartBtn.parentElement.querySelector(".price").textContent;
+		const plan = cartBtn.parentElement.querySelector(".plan-name").textContent;
+		let cartPlans = JSON.parse(localStorage.getItem("cartPlans")) || [];
+		cartPlans.push({ plan, price });
+		localStorage.setItem("cartPlans", JSON.stringify(cartPlans));
+
+		// console.log(cartPlans);
+	});
+});
